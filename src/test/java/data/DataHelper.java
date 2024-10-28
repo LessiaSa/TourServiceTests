b5197ca08662dafb40af093832f4bb8d35cb0f16
LessiaSa <0162221@mail.ru>
2024-10-28 12:19:51 +0700

    NewService

---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,116 @@
+package data;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataHelper {
+
+    private DataHelper () {
+    }
+
+    public static String getApprovedNumber() {
+        return "111 2222 3333 4444";
+    }
+
+    public static String getDeclinedNumber() {
+        return "5555 6666 7777 8888";
+    }
+
+    public static String getCurrentMonth() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%02d", localDate.getMonthValue());
+    }
+
+
+    public static String getLastMonth() {
+        LocalDate localDate = LocalDate.now();
+        LocalDate lastMonth = localDate.minusMonths(1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+        String monthValue = lastMonth.format(formatter);
+        return monthValue;
+    }
+
+    public static String getCurrentYear() {
+        return String.format("%ty", Year.now());
+    }
+
+    public static String getLastYear() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%ty", localDate.minusYears(1));
+    }
+
+    public static String getNextYear() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%ty", localDate.plusYears(1));
+    }
+
+    public static String getNameRu() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName();
+    }
+
+    public static String getNameEn() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().firstName();
+    }
+
+    public static String getSurnameRu() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().lastName();
+    }
+
+    public static String getSurnameEn() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().lastName();
+    }
+
+
+    public static String getRandomUserEn() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    private static String getRandomUserRu() {
+        Faker faker= new Faker(new Locale("ru"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String getTooLongName() {
+        Faker faker = new Faker();
+        return faker.lorem().fixedString(200);
+    }
+
+    public static String getNumbersUsed() {
+        Faker faker = new Faker();
+        return faker.number().digits(9);
+    }
+
+    public static String getNameWithOneLetter() {
+        Faker faker = new Faker();
+        return faker.lorem().characters(1);
+    }
+
+    public static String getValidCvc() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
+        return fakeValuesService.numerify("###");
+    }
+
+    public static String getCvcWithOneDigit() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
+        return fakeValuesService.numerify("#");
+    }
+
+    public static String getCvcWithTwoDigits() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en"), new RandomService());
+        return fakeValuesService.numerify("##");
+    }
+}
---
+++ src/test/java/tests/CreditPageTests.java
@@ -0,0 +1,40 @@
+package tests;
+
+import data.Card;
+import data.SQLHelper;
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import page.CreditPage;
+import page.StartPage;
+
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.DataHelper.*;
+
+public class CreditPageTests {
+
+    @BeforeEach
+    void setUp() {
+      SQLHelper.clearTables();
+      String url = System.getProperty("sut.url");
+      open(url);
+    }
+
+    @Test
+    @Order(1)
+    void shouldSuccessNotification() throws SQLException {
+        Card card = new Card(getApprovedNumber(), getCurrentMonth(), getNextYear(), getValidCvc(), getRandomUserEn());
+        val startPage = new StartPage();
+        startPage.buyInCredit();
+        val creditPage = new CreditPage();
+        creditPage.detailsCard(card);
+        creditPage.checkSuccessNotification();
+    }
+
+
+
+}
